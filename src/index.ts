f34e4565cea85560965de348f328fd1382b9419f
BartoszBugla <48729791+BartoszBugla@users.noreply.github.com>
2024-03-15 23:04:16 +0100

    Changed port

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(80);
+const app = new Elysia().get("/", () => "Hello Elysia").listen(443);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
